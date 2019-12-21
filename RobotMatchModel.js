b6cafd31b1222c09ddf14d4a8c39b45ceca5814f
efeakinci <2021akinciefey@aaps.k12.mi.us>
2019-12-21 18:51:38 -0500

    Initital Backend Commit

---
+++ RobotMatchModel.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+
+let RobotMatchModel = new mongoose.Schema({
+    matchNumber: Number,
+    robot: {
+        teamNumber: Number,
+        allianceNumber: Number,
+        allianceColor: String
+    },
+    stats: {
+
+    }
+});
+
+RobotMatchModel.index({'matchNumber': 1, 'robot.teamNumber': 1}, { unique: true });
+
+
+module.exports = mongoose.model('RobotMatch', RobotMatchModel);
---
+++ backend.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose');
+let RobotMatchModel = require("./RobotMatchModel");
+mongoose.connect("mongodb+srv://rouser:nbBvbtUDROLXc6ib@cluster0-1kgz4.mongodb.net/test?retryWrites=true&w=majority").then(()=>{
+    console.log("Connected")
+    })
+    .catch((error)=>{
+        console.log("Not connected. Error: " + error);
+    });
+
+
+
+
+
+
+function createRecord(incJson) {
+    new RobotMatchModel({
+        matchNumber: incJson.matchNumber,
+        robot: {
+            teamNumber: incJson.robot.teamNumber,
+            allianceColor: incJson.robot.allianceColor,
+            allianceNumber: incJson.robot.allianceNumber
+        }
+    }).save().catch(() => {
+        console.log("Something went wrong while saving. Possible duplicate?")
+    });
+}
+
+function findTeamByMatchNumber(teamNumber, matchNumber) {
+    RobotMatchModel.find({'robot.teamNumber': teamNumber, matchNumber: matchNumber}).then(found=>{console.log(found)});
+}
+
+function findTeamsInMatch(matchNumber) {
+    RobotMatchModel.find({matchNumber: matchNumber}).then(found=>{console.log(found)});
+}
+
+findTeamByMatchNumber(3322, 1);
