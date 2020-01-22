160ca132c096216ed479076f54fd217810dac751
Efe Akinci <efeakinci@Efes-MacBook-Pro.local>
2020-01-21 20:15:47 -0500

    Added toggling for charts. UI improvements.

--- frontend/src/Components/User-Components/Statistics/TeamStatistics.js
+++ frontend/src/Components/User-Components/Statistics/TeamStatistics.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Container, Row, Col, Dropdown, DropdownButton } from 'react-bootstrap';
 import './TeamStatistics.css';
 import DropdownItem from 'react-bootstrap/DropdownItem';
-import { Line, Doughnut } from "react-chartjs-2";
+import StatisticItem from "./StatsticItem";
 
 export default class TeamStatistics extends Component {
 
@@ -55,71 +55,17 @@ export default class TeamStatistics extends Component {
         if (mCollection !== undefined) {
             if (mCollection.length > 0) {
                 let itemName = "";
-                let nItems = 0;
                 let outputData = [];
 
                 for (let i = 0; i < mCollection[0].length; i++) {
-                    switch (mCollection[0][i].chartType) {
-
-                        case "L":
-                            mCollection.forEach(match => {
-                                outputData.push(match[i].dataValue);
-                                nItems++;
-                                itemName = match[i].name;
-                            });
-                            output.push(<Row key={i} className="statsRow"><Col xs="6" md="4"><h4>{itemName}</h4></Col><Col xs="6" md="2"><h4>{(outputData.reduce((total, a) => total + a) / nItems).toFixed(2)}</h4></Col><Col md="6"><Line data={{labels: [...Array(outputData.length).keys()].map(element => "Match " + (element + 1)), datasets: [{
-                                label: itemName,
-                                data: outputData,
-                                backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)'
-                                ],
-                                borderColor: [
-                                    'rgba(255, 99, 132, 1)'
-                                ],
-                                borderWidth: 1,
-                                fill: false
-                            }]}} options={{
-                                scales: {
-                                    yAxes: [{
-                                        ticks: {
-                                            beginAtZero: true
-                                        }
-                                    }]
-                                }
-                            }} /> </Col></Row>);
-                            outputData = [];
-                            nItems = 0;
-                            break;
-                        
-                        case "O":
-                            // Do pie chart stuff
-                            mCollection.forEach(match => {
-                                outputData.push(match[i].dataValue);
-                                nItems++;
-                                itemName = match[i].name;
-                            });
-                            let doughnutData = [0, 0];
-                            outputData.forEach(element => (element === true) ? doughnutData[0]++ : doughnutData[1]++);
-                            
-                            output.push(<Row key={i} className="statsRow"><Col xs="6" md="4"><h4>{itemName}</h4></Col><Col className="itemCol" xs="6" md="2"><h4>{(outputData.reduce((total, a) => total + a) / nItems * 100).toFixed(2)}%</h4></Col><Col xs="12" md="6"><Doughnut data={{labels: ["Did the thing", "Didn't do the thing"], datasets: [{
-                                data: doughnutData,
-                                backgroundColor: [
-                                    'rgba(66, 245, 135, 0.2)',
-                                    'rgba(255, 99, 132, 0.2)'
-                                ],
-                                borderColor: [
-                                    'rgba(61, 219, 122, 1)',
-                                    'rgba(255, 99, 132, 1)'
-                                ],
-                                borderWidth: 1,
-                                fill: false
-                            }]}} /> </Col></Row>);
-                            outputData = [];
-                            nItems = 0;
-                            break;
-                        default:
-                            output.push(<Row key={i}><Col>Unknown data type</Col></Row>);
-                    }
+                    mCollection.forEach(match => {
+                        outputData.push(match[i].dataValue);
+                        itemName = match[i].name;
+                    });
+                    output.push(<StatisticItem itemName = {itemName} chartType = {mCollection[0][i].chartType} data={outputData} i = {i} />);
+                    // Schema mCollection[0][i]
+                    // match[i]
+                    outputData = [];
                 }
 
                 }
