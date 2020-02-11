ecc0304ea6179cb0da12ed8b1102a6fc1a8d7f3b
Efe Akinci <efeakinci@Efes-MacBook-Pro.local>
2020-02-11 18:16:13 -0500

    Removed robot object

--- backend/backend.js
+++ backend/backend.js
@@ -23,12 +23,6 @@ let db = admin.firestore();
 
 const scoutMatches = db.collection('scoutmatches');
 
-scoutMatches.doc({thing: }).set({
-    hi: "jonathan"
-});
-
-
-
 /*
 {
         matchNumber: `i`ncJson.matchNumber,
@@ -48,15 +42,7 @@ scoutMatches.doc({thing: }).set({
 
 
 function createRecord(incJson) {
-    new RobotMatchModel(incJson).save().catch(() => {
-        console.log("Something went wrong while saving. Possible duplicate?")
-    });
-}
-
-function updateTeam(query, stats) {
-    return new Promise ((resolve, reject) => {
-        RobotMatchModel.findOneAndUpdate(query, {"$set": {stats}}, (err, doc) => {(err) ? reject(err) : resolve(doc)});
-    });
+    scoutMatches.doc("t"+incJson.robot.teamNumber+"m"+incJson.matchNumber).set(JSON.parse(JSON.stringify(incJson)));
 }
 
 app.get('/create-match/:matchNumber', (req, res) => {
@@ -64,11 +50,9 @@ app.get('/create-match/:matchNumber', (req, res) => {
         {
             matchNumber: req.params.matchNumber,
             won: true,
-            robot: {
-                teamNumber: 6429,
-                allianceColor: "red",
-                allianceNumber: 1
-            },
+            teamNumber: 6429,
+            allianceColor: "red",
+            allianceNumber: 1,
             stats: {
                 auto: [
                     new Statistic("Moved", Math.random() > .5, 'O'),
@@ -96,18 +80,29 @@ app.get('/create-match/:matchNumber', (req, res) => {
 
 app.get('/getallscoutentries/:skip', (req, res) => {
     //TODO Implement Search By Last ID instead. The .skip method won't scale.
-    let searchParams = {}
+    let search = scoutMatches;
     if (req.query.teamNumber !== '' && req.query.teamNumber !== undefined) {
-        searchParams['robot.teamNumber'] = req.query.teamNumber;
+        search = search.where('teamNumber', '==', req.query.teamNumber);
     }
     if (req.query.matchNumber !== '' && req.query.matchNumber !== undefined) {
-        searchParams['matchNumber'] = req.query.matchNumber;
+        //search = search.where('matchNumber', '==', req.query.matchNumber);
+
     }
+    search.get().then(snapshot => {
+        if (snapshot.empty) {
+          console.log('No matching documents.');
+          return;
+        }  
     
-    let query = (!req.params.skip.isNaN) ? RobotMatchModel.find(searchParams).limit(10).skip(parseInt(req.params.skip)) : RobotMatchModel.find(searchParams).limit(10);
+        snapshot.forEach(doc => {
+          console.log(doc.id, '=>', doc.data());
+        });
+      })
+      .catch(err => {
+        console.log('Error getting documents', err);
+      });
     
-    return query.then(doc => res.send(doc)).catch(err=>console.log(err));
-});
+        });
 
 function populateMatches(eventKey) {
     axios.get("https://www.thebluealliance.com/api/v3/event//matches/simple", {"X-TBA-Auth-Key": "84zuY6dS8oOnAyKc09C59oxVfuOwscn6Ak6EXrChraCjY6DNmdFgJOSXX0lLBfMd"})
